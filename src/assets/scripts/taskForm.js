5babf949661cc500ad174bc9487b90d656116696
Alexey <darth.tesla47@gmail.com>
2020-04-18 18:24:54 +0300

    add task adder script

---
+++ src/assets/scripts/taskForm.js
@@ -0,0 +1,41 @@
+const form = document.querySelector(".form");
+const taskNameInput = document.querySelector(".form__task-name");
+const taskDescInput = document.querySelector(".form__task-desc");
+const addTaskButton = document.querySelector(".form__btn");
+const taskCardsContainer = document.querySelector(".tasks-section__container");
+const taskCardButtonsPattern = `<a class="task-card__complete" href="#"> Выполнено</a>
+                                <a class="task-card__edit" href="#"> Редактировать</a>
+                                <a class="task-card__remove" href="#"> Удалить</a>`;
+
+addTaskButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (taskNameInput.value !== "" && taskDescInput.value !== "") {
+    return addNewTask(taskNameInput.value, taskDescInput.value);
+  }
+});
+
+function addNewTask(taskName, taskDesc) {
+  const fragment = document.createDocumentFragment();
+
+  const card = document.createElement("div");
+  const name = document.createElement("h3");
+  const desc = document.createElement("p");
+  const buttons = document.createElement("div");
+
+  card.classList.add("task-card");
+  name.classList.add("task-card__task-name");
+  desc.classList.add("task-card__task-desc");
+  buttons.classList.add("task-card__btns");
+
+  name.textContent = taskName;
+  desc.textContent = taskDesc;
+  buttons.innerHTML = taskCardButtonsPattern;
+
+  card.appendChild(name);
+  card.appendChild(desc);
+  card.appendChild(buttons);
+
+  fragment.appendChild(card);
+
+  taskCardsContainer.appendChild(fragment);
+}
